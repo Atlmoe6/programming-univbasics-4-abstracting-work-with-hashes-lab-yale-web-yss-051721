b58bdc06648e6072f926e65366e0033aeab0d9bb
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-29 03:23:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,6 +4,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
+  {location: 'New Haven', occupation: 'student'}
   # return the correct value using the hash and key parameters
 end
 
